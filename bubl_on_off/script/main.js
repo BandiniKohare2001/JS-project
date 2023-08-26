f9d5dde9a7873cfe8490b7f8487c7951ac83a6e6
harshvardhan-kh <harshvardhan3298@gmail.com>
2023-08-26 13:19:16 +0530

    javascript function added

--- bubl_on_off/script/main.js
+++ bubl_on_off/script/main.js
@@ -1,5 +1,28 @@
-const blink = document.getElementById ("main-cover")
-function blink()
-{
 
+function onbulb()
+{
+   document.getElementById("bulb-change").src="./img/yellow.png" 
+} 
+function offbulb()
+{
+   document.getElementById("bulb-change").src="./img/normal.png" 
+} 
+
+function redbulb()
+{
+   document.getElementById("bulb-change").src="./img/red.png" 
+} 
+function pinkbulb()
+{
+   document.getElementById("bulb-change").src="./img/pink.png" 
+} 
+
+function orangebulb()
+{
+   document.getElementById("bulb-change").src="./img/orange.png" 
+} 
+
+function cyanebulb()
+{
+   document.getElementById("bulb-change").src="./img/cyane.png" 
 } 
